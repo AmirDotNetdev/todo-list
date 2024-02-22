dceb3cf47da309cb21fb361b3b8109e040015b79
amirdotnetdev <amirkamyabi4@gmail.com>
2024-02-22 04:58:04 -0500

    commit

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
+    
+    
+    path('login/' , views.CustomLoginView.as_view(), name = 'login' ),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+
     path('', views.TaskList.as_view(), name = 'tasks'),
     path('task/<int:pk>/', views.TaskDetail.as_view(), name = 'task'),
     path('task-update/<int:pk>/', views.TaskUpdate.as_view(), name = 'update'),
--- base/views.py
+++ base/views.py
@@ -6,25 +6,39 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
 from .models import Task
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin ,ListView):
     model = Task
     context_object_name = 'tasks'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user = self.request.user)
+        context['count'] = context['tasks'].filter(complete = False).count()
+        
+        return context
 
-class TaskDetail(DetailView):
+    
+
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
 
 class TaskCreate(CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title' , 'description', 'complete']
     success_url = reverse_lazy('tasks')
+    
+    def form_valid(self,form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
 
 class TaskUpdate(UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title' , 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 class TaskDelete(DeleteView):
